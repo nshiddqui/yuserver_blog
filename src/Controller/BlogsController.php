a2f4c2d4b708da62f958c2bd77df39da40ed21a3
Nazim <nazim@adaptabiz.com>
2020-07-13 01:53:00 +0530

    fix issue with notification

--- src/Controller/BlogsController.php
+++ src/Controller/BlogsController.php
@@ -64,7 +64,7 @@ class BlogsController extends AppController {
                     'contain' => ['BlogContents', 'Comments'],
                 ])->first();
         $this->loadModel('BlogContents');
-        $this->BlogContents->updateAll('views = views + 1', array('slug' => $slug));
+        $this->BlogContents->updateAll('views = views + 1', array('blog_id' => $blog->id));
         $recent_blogs = $this->Blogs->find('all', [
             'order' => [
                 'Blogs.created'
