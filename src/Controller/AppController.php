6db149b1256e3df3ce33bd2048e38a7d05674cb6
Nazim <nazim@adaptabiz.com>
2021-06-18 04:29:20 +0530

    commit

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -54,7 +54,7 @@ class AppController extends Controller {
                 ]
             ]
         ]);
-        $this->Auth->setConfig('authorize', ['Home','Blogs']);
+        $this->Auth->setConfig('authorize', ['Controller' => ['Home', 'Blogs']]);
         /*
          * Enable the following component for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
